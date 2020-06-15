610634696352c05c5e3bcca57d5dc7b54c1fbf24
guillaumedsde <guillaume@desusanne.com>
2020-06-15 15:47:22 +0200

    ci: added build scripts

---
+++ build/build.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+cd ~
+
+# install build script dependencies
+apk add --no-cache bash wget curl
+
+# download build script
+wget -qO ~/qbittorrent-nox-static-musl.sh https://git.io/JvLcZ
+chmod 700 ~/qbittorrent-nox-static-musl.sh
+
+# install build dependencies
+~/qbittorrent-nox-static-musl.sh
+
+# build static qbittorrent binary
+~/qbittorrent-nox-static-musl.sh all
+
+# install static qbittorrent binary to /usr/local/bin
+~/qbittorrent-nox-static-musl.sh install
+
+# copy built binary to the build volume
+cp /usr/local/bin/qbittorrent-nox /build
