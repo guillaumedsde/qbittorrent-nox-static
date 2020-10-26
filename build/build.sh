1e2187c4ecb522eeac9ca5e6d184448244606df9
guillaumedsde <guillaume@desusanne.com>
2020-10-26 09:44:21 +0100

    fix: use build script in all CI

--- build/build.sh
+++ build/build.sh
@@ -1,22 +1,22 @@
 #!/bin/sh
 
-cd ~
+echo "building in $(pwd)"
 
 # install build script dependencies
 apk add --no-cache bash wget curl
 
 # download build script
-wget -qO ~/qbittorrent-nox-static-musl.sh https://git.io/JvLcZ
-chmod 700 ~/qbittorrent-nox-static-musl.sh
+wget -qO ./qbittorrent-nox-static-musl.sh https://git.io/JvLcZ
+chmod 700 ./qbittorrent-nox-static-musl.sh
 
 # install build dependencies
-~/qbittorrent-nox-static-musl.sh
+./qbittorrent-nox-static-musl.sh
 
 # build static qbittorrent binary
-~/qbittorrent-nox-static-musl.sh all -b "/root/qbittorrent-build" --libtorrent-master v2.0.0 --qbittorrent-master release-4.3.0
+./qbittorrent-nox-static-musl.sh all -b "$(pwd)/qbittorrent-build" --libtorrent-master v2.0.0 --qbittorrent-master release-4.3.0
 
 # install static qbittorrent binary to /usr/local/bin
-~/qbittorrent-nox-static-musl.sh install -b "/root/qbittorrent-build"
+./qbittorrent-nox-static-musl.sh install -b "$(pwd)/qbittorrent-build"
 
 # copy built binary to the build volume
 cp /usr/local/bin/qbittorrent-nox /build
