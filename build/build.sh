0fa7fd139e8a845f8a95150f0e0475c6f5dd048a
guillaumedsde <guillaume@desusanne.com>
2020-11-22 19:10:46 +0100

    fix: build script

--- build/build.sh
+++ build/build.sh
@@ -6,17 +6,17 @@ echo "building in $(pwd)"
 apk add --no-cache bash wget curl
 
 # download build script
-wget -qO ./qbittorrent-nox-static-musl.sh https://git.io/JvLcZ
-chmod 700 ./qbittorrent-nox-static-musl.sh
+wget -qO ./qbittorrent-nox-static.sh https://git.io/JvLcZ
+chmod 700 ./qbittorrent-nox-static.sh
 
 # install build dependencies
-./qbittorrent-nox-static-musl.sh
+./qbittorrent-nox-static.sh
 
 # build static qbittorrent binary
-./qbittorrent-nox-static-musl.sh all -b "$(pwd)/qbittorrent-build"
+./qbittorrent-nox-static.sh all -b "$(pwd)/qbittorrent-build"
 
 # install static qbittorrent binary to /usr/local/bin
-./qbittorrent-nox-static-musl.sh install -b "$(pwd)/qbittorrent-build"
+./qbittorrent-nox-static.sh install -b "$(pwd)/qbittorrent-build"
 
 # copy built binary to the build volume
 cp /usr/local/bin/qbittorrent-nox "$(pwd)"
