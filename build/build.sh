06a7f64cf37059da0519024cd0d6057d880e9e43
guillaumedsde <guillaume@desusanne.com>
2020-12-10 22:22:40 +0100

    fix: install deps before building, use bash for script

--- build/build.sh
+++ build/build.sh
@@ -3,7 +3,7 @@
 QBITTORRENT_VERSION=release-4.3.1
 LIBTORRENT_VERSION=v2.0.1
 
-echo "building in $(pwd)"
+echo "building qBittorrent ${QBITTORRENT_VERSION} with libtorrent ${LIBTORRENT_VERSION} in $(pwd)"
 
 # install build script dependencies
 apk add --no-cache bash wget curl
@@ -12,11 +12,14 @@ apk add --no-cache bash wget curl
 wget -qO ./qbittorrent-nox-static.sh https://git.io/JvLcZ
 chmod 700 ./qbittorrent-nox-static.sh
 
-# install build dependencies and build static qbittorrent binary
-./qbittorrent-nox-static.sh all -b "$(pwd)/qbittorrent-build" --libtorrent-tag "${LIBTORRENT_VERSION}" --qbittorrent-tag "${QBITTORRENT_VERSION}" --optimize
+# install build dependencies
+bash ./qbittorrent-nox-static.sh
+
+# build static qbittorrent binary
+bash ./qbittorrent-nox-static.sh all -b "$(pwd)/qbittorrent-build" --libtorrent-tag "${LIBTORRENT_VERSION}" --qbittorrent-tag "${QBITTORRENT_VERSION}" --optimize
 
 # install static qbittorrent binary to /usr/local/bin
-./qbittorrent-nox-static.sh install -b "$(pwd)/qbittorrent-build"
+bash ./qbittorrent-nox-static.sh install -b "$(pwd)/qbittorrent-build"
 
 # copy built binary to the build volume
 cp /usr/local/bin/qbittorrent-nox "$(pwd)"
